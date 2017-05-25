e66d489ce88163f77896b450151edf2f53925e6a
MBaran88 <zdzibq@gmail.com>
2017-05-25 19:44:02 +0200

    commit #1

---
+++ src/com/PESELSet/Main.java
@@ -0,0 +1,8 @@
+package com.PESELSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/PESELSet/PeselSet.java
@@ -0,0 +1,10 @@
+package com.PESELSet;
+
+/**
+ * Created by RENT on 2017-05-25.
+ */
+public class PeselSet {
+    private PeselSet firstitem;
+    private PeselSet lastitem;
+    private int size;
+}
