7c0ede930b5b52ee0d895459044ba073ec5478eb
MBaran88 <zdzibq@gmail.com>
2017-05-26 18:24:10 +0200

    Coś ruszyło ale chuj wie co teraz zrobić

--- src/com/PESELSet/Main.java
+++ src/com/PESELSet/Main.java
@@ -1,11 +1,28 @@
 package com.PESELSet;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
+        // zrobić zbiór który przyjmuje unikatowe wartości PESEL
+        // Creating a Set with PESEL numerals
+
+        Set<String> PeselList = new TreeSet<String>();
+        PeselList = new HashSet<String>(Arrays.asList("8","8","1","1","2","7","1","1","3","1","7"));
+        int foo = Integer.parseInt(PeselList);
+
+
+
+        System.out.println(PeselList);
+    }
+
 
 
     }
-}
+
